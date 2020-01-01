30964161c37cd4b9846ee5ab972c809f9d7ef5aa
DuckLuckBreakout <mrdivan07@gmail.com>
2020-01-01 16:31:39 +0300

    Add solve

--- practice_7/Systems_of_linear_algebraic_equations/3.py
+++ practice_7/Systems_of_linear_algebraic_equations/3.py
@@ -4,15 +4,13 @@ A = np.array([[1, 2, 3],
               [4, 5, 6],
               [7, 8, 9]])
 
-# 0 решений
-# B = np.array([[12, 2, 1]])
-
-# Бесконечное число решений
-B = np.array([[88, 92, 96]])
+B = np.array([[12, 2, 1]])
 
 C = np.concatenate((A, B.T), axis=1)
 if np.linalg.matrix_rank(A, 0.0001) < np.linalg.matrix_rank(C, 0.0001):
     print("Не имеет решений")
+    B = np.array([88, 92, 96])
+    print(np.linalg.solve(A, B))
 elif np.linalg.matrix_rank(A, 0.0001) == np.linalg.matrix_rank(C, 0.0001):
     if np.linalg.matrix_rank(A, 0.0001) < len(A[0]):
         print("Бесконечное число решений")
